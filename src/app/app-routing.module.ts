67f82964622d3f080d44587770c946980ba291ab
refazul <refazul.refat@yahoo.com>
2020-11-14 14:25:01 +0000

    first route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListingPageComponent } from './listing-page/listing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listings', component: ListingPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingPageComponent
   ],
   imports: [
     BrowserModule,
