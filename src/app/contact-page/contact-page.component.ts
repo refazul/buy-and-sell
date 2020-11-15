ab7a53813610e76049f83ddfe6c3dc1fc27030ad
refazul <refazul.refat@yahoo.com>
2020-11-15 03:27:56 +0000

    listing-detail-page

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ListingsService } from '../listings.service';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  listing: Listing | undefined;
+  message: string | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private listingsService: ListingsService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.listingsService.getListingById(id)
+      .subscribe(listing => {
+        this.listing = listing;
+        this.message = `I'm interested in you ${this.listing.name}`;
+      });
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/');
   }
 
 }
--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListingsService } from '../listings.service';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listing-detail-page.component.css']
 })
 export class ListingDetailPageComponent implements OnInit {
+  isLoading: boolean = true;
+  listing: Listing = { id: '', name: '', description: '', price: 0, views: 0 };
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private listingService: ListingsService
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.listingService.getListingById(id)
+      .subscribe(listing => {
+        this.listing = listing;
+        this.isLoading = false;
+      });
+    this.listingService.addViewToListing(id)
+      .subscribe(() => {
+        console.log("Views updated!");
+      });
   }
 
 }
