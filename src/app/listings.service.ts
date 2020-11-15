1d2ad183bf9724e3938764002b0fb3ac220f7539
refazul <refazul.refat@yahoo.com>
2020-11-15 18:32:51 +0000

    get real user listing

--- src/app/listings.service.ts
+++ src/app/listings.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Listing } from './types';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json'
   })
 }
+const httpOptionsWithAuthToken = (token: string) => ({
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'AuthToken': token,
+  })
+});
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +22,8 @@ const httpOptions = {
 export class ListingsService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private auth: AngularFireAuth
   ) { }
 
   getListings(): Observable<Listing[]> {
@@ -35,9 +43,20 @@ export class ListingsService {
   }
 
   getListingsForUser(): Observable<Listing[]> {
-    return this.http.get<Listing[]>(
-      `/api/users/12345/listings`
-    );
+    return new Observable<Listing[]>(observer => {
+      this.auth.user.subscribe(user => {
+        user && user.getIdToken().then(token => {
+          if (user && token) {
+            this.http.get<Listing[]>(`/api/users/${user.uid}/listings`, httpOptionsWithAuthToken(token))
+              .subscribe(listings => {
+                observer.next(listings);
+              });
+          } else {
+            observer.next([]);
+          }
+        })
+      })
+    })
   }
 
   deleteListing(id: string): Observable<any> {
